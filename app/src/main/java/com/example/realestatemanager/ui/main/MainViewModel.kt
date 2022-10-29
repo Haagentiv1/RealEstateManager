fda54cf99c184290f5085fea3941863aff0e51eb
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-10-29 23:37:14 +0200

    feat : add main viewModel and fun to handle event from main activity and rotation changed

---
+++ app/src/main/java/com/example/realestatemanager/ui/main/MainViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.realestatemanager.ui.main
+
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.ViewModel
+import com.example.realestatemanager.ui.utils.SingleLiveEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class MainViewModel : ViewModel() {
+
+    private var isTablet: Boolean = false
+    val state = MediatorLiveData<String>().apply {
+        value = ""
+    }
+    val navigateSingleLiveEvent = SingleLiveEvent<MainViewAction>()
+
+
+    init {
+        navigateSingleLiveEvent.addSource(state) {
+            if (!isTablet) {
+                navigateSingleLiveEvent.setValue(MainViewAction.NavigateToCreatePropertyActivity)
+            }
+        }
+    }
+
+    fun onConfigurationChanged(isTablet: Boolean) {
+        this.isTablet = isTablet
+    }
+
+}
