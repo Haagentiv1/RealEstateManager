57f16592a843c92bf902c805e78863c372b62353
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-10-29 16:55:11 +0200

    feat : setup PropertyDetailFragment and PropertyListFragment

--- app/src/main/java/com/example/realestatemanager/ui/propertyDetail/PropertyDetailFragment.kt
+++ app/src/main/java/com/example/realestatemanager/ui/propertyDetail/PropertyDetailFragment.kt
@@ -1,6 +1,23 @@
 package com.example.realestatemanager.ui.propertyDetail
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.realestatemanager.databinding.PropertyDetailFragmentBinding
 
 class PropertyDetailFragment : Fragment() {
+
+    var _binding : PropertyDetailFragmentBinding? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = PropertyDetailFragmentBinding.inflate(inflater,container,false)
+        return binding.root
+    }
 }
--- app/src/main/java/com/example/realestatemanager/ui/propertyList/PropertyListFragment.kt
+++ app/src/main/java/com/example/realestatemanager/ui/propertyList/PropertyListFragment.kt
@@ -1,6 +1,24 @@
 package com.example.realestatemanager.ui.propertyList
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.realestatemanager.databinding.PropertyDetailFragmentBinding
 
 class PropertyListFragment : Fragment() {
+    private var _binding: PropertyDetailFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = PropertyDetailFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
 }
