2701cdbf721d58f53022454fdf5468df8fa16f6c
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-10-29 19:55:26 +0200

    fix (PropertyListFragment) : right binding value fixe

--- app/src/main/java/com/example/realestatemanager/ui/propertyList/PropertyListFragment.kt
+++ app/src/main/java/com/example/realestatemanager/ui/propertyList/PropertyListFragment.kt
@@ -6,9 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.realestatemanager.databinding.PropertyDetailFragmentBinding
+import com.example.realestatemanager.databinding.PropertyListFragmentBinding
 
 class PropertyListFragment : Fragment() {
-    private var _binding: PropertyDetailFragmentBinding? = null
+    private var _binding: PropertyListFragmentBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -16,7 +17,7 @@ class PropertyListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = PropertyDetailFragmentBinding.inflate(inflater, container, false)
+        _binding = PropertyListFragmentBinding.inflate(inflater,container,false)
         return binding.root
     }
 
