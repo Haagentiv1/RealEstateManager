27caa9956360b35b9b23e02ea6aaec891b2fceb0
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-10-29 23:38:10 +0200

    feat : handle navigation event

--- app/src/main/java/com/example/realestatemanager/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/realestatemanager/ui/main/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.realestatemanager.ui.main
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.example.realestatemanager.R
 import com.example.realestatemanager.databinding.MainActivityBinding
 import com.example.realestatemanager.ui.propertyDetail.PropertyDetailFragment
 import com.example.realestatemanager.ui.propertyList.PropertyListFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,13 +20,16 @@ class MainActivity : AppCompatActivity() {
         val binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if (savedInstanceState == null){
+        if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(binding.mainFlContainerPropertyList.id,PropertyListFragment())
+                .replace(binding.mainFlContainerPropertyList.id, PropertyListFragment())
                 .commitNow()
         }
 
-        if (binding.mainFlContainerPropertyDetail != null && supportFragmentManager.findFragmentById(binding.mainFlContainerPropertyDetail.id) == null){
+        if (binding.mainFlContainerPropertyDetail != null && supportFragmentManager.findFragmentById(
+                binding.mainFlContainerPropertyDetail.id
+            ) == null
+        ) {
             supportFragmentManager.beginTransaction()
                 .add(
                     binding.mainFlContainerPropertyDetail.id,
@@ -28,8 +37,19 @@ class MainActivity : AppCompatActivity() {
                 )
                 .commitNow()
         }
+        viewModel.navigateSingleLiveEvent.observe(this){
+            when(it){
+                MainViewAction.NavigateToCreatePropertyActivity -> TODO()
+                MainViewAction.NavigateToPropertyDetailActivity -> TODO()
+                MainViewAction.NavigateToPropertyMapExplorerActivity -> TODO()
+                MainViewAction.NavigateToRealEstateLoanActivity -> TODO()
+            }
+        }
 
+    }
 
-
+    override fun onResume() {
+        super.onResume()
+        viewModel.onConfigurationChanged(resources.getBoolean(R.bool.isTablet))
     }
 }
