bddbf2e9e7db5474d9118348bc32dd56c84d09f4
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-10-30 05:08:29 +0100

    fix : add @inject annotation

--- app/src/main/java/com/example/realestatemanager/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/realestatemanager/ui/main/MainViewModel.kt
@@ -4,9 +4,10 @@ import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import com.example.realestatemanager.ui.utils.SingleLiveEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel : ViewModel() {
+class MainViewModel @Inject constructor() : ViewModel() {
 
     private var isTablet: Boolean = false
     val state = MediatorLiveData<String>().apply {
