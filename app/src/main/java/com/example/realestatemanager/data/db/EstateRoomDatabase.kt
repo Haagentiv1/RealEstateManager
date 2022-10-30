11ca14b504ce3f3d517d63f0d727d4f5856c0662
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-10-30 20:48:52 +0100

    feat : create database class and build construction/migration logic

--- app/src/main/java/com/example/realestatemanager/data/db/EstateRoomDatabase.kt
+++ app/src/main/java/com/example/realestatemanager/data/db/EstateRoomDatabase.kt
@@ -1,11 +1,33 @@
 package com.example.realestatemanager.data.db
 
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.realestatemanager.data.DAO.EstateDao
 import com.example.realestatemanager.data.model.Estate
 
 @Database(entities = [Estate::class], version = 1, exportSchema = false)
 abstract class EstateRoomDatabase : RoomDatabase() {
+    abstract fun estateDao(): EstateDao
 
+    companion object {
+        @Volatile
+        private var INSTANCE: EstateRoomDatabase? = null
+        fun getDatabase(context: Context): EstateRoomDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance =
+                    Room.databaseBuilder(
+                        context.applicationContext,
+                        EstateRoomDatabase::class.java,
+                        "estate_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
