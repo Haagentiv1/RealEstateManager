3a32a2eb6a412256df2d96204b454d883dcc8697
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-11-06 23:27:09 +0100

    ref  : database building logic moved inside Hilt App module

--- app/src/main/java/com/example/realestatemanager/data/db/EstateDatabase.kt
+++ app/src/main/java/com/example/realestatemanager/data/db/EstateDatabase.kt
@@ -7,23 +7,24 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.realestatemanager.data.Converters
-import com.example.realestatemanager.data.DAO.EstateDao
+import com.example.realestatemanager.data.Dao.EstateDao
 import com.example.realestatemanager.data.model.Estate
 
 @TypeConverters(Converters::class)
 @Database(entities = [Estate::class], version = 1, exportSchema = false)
-abstract class EstateRoomDatabase : RoomDatabase() {
-    abstract fun estateDao(): EstateDao
+abstract class EstateDatabase : RoomDatabase() {
+    abstract val estateDao: EstateDao
 
     companion object {
+        const val DATABASE_NAME = "estate_database"
         @Volatile
-        private var INSTANCE: EstateRoomDatabase? = null
-        fun getDatabase(context: Context): EstateRoomDatabase {
+        private var INSTANCE: EstateDatabase? = null
+        fun getDatabase(context: Context): EstateDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance =
                     Room.databaseBuilder(
                         context.applicationContext,
-                        EstateRoomDatabase::class.java,
+                        EstateDatabase::class.java,
                         "estate_database"
                     )
                         .fallbackToDestructiveMigration()
