bf58847460d1382a165afb7942cf04ca79300105
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-11-06 23:28:32 +0100

    feat  : add glide and binding logic

--- app/src/main/java/com/example/realestatemanager/ui/propertyList/EstateListAdapter.kt
+++ app/src/main/java/com/example/realestatemanager/ui/propertyList/EstateListAdapter.kt
@@ -5,12 +5,15 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
 import com.example.realestatemanager.data.model.Estate
 import com.example.realestatemanager.databinding.PropertyItemListFragmentBinding
+import javax.inject.Inject
 
-class EstateListAdapter(
+class EstateListAdapter @Inject constructor(
+    private val glide: RequestManager,
     private val listener: (id: String) -> Unit
-) : ListAdapter<Estate, EstateListAdapter.EstateViewHolder>(EstateComparator()) {
+) : ListAdapter<EstateListItemViewState, EstateListAdapter.EstateViewHolder>(EstateComparator()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EstateViewHolder =
         EstateViewHolder(
@@ -22,21 +25,27 @@ class EstateListAdapter(
         )
 
     override fun onBindViewHolder(holder: EstateViewHolder, position: Int) {
-
+        holder.bind(getItem(position), listener,glide)
     }
 
     class EstateViewHolder(private val binding: PropertyItemListFragmentBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: EstateListItemViewState, listener: (id: String) -> Unit) {
-
+        fun bind(item: EstateListItemViewState, listener: (id: String) -> Unit,glide: RequestManager) {
+            glide.load(item.picture).into(binding.propertyItemTvPropertyPicture)
+            binding.propertyItemTvPropertyType.text = item.type
+            binding.propertyItemTvPropertyTown.text = item.town
+            binding.propertyItemTvPropertyPrice.text = item.price
+            binding.root.setOnClickListener {
+                listener.invoke(item.id)
+            }
         }
     }
 
-    class EstateComparator : DiffUtil.ItemCallback<Estate>() {
-        override fun areContentsTheSame(oldItem: Estate, newItem: Estate): Boolean =
+    class EstateComparator : DiffUtil.ItemCallback<EstateListItemViewState>() {
+        override fun areContentsTheSame(oldItem: EstateListItemViewState, newItem: EstateListItemViewState): Boolean =
             oldItem.id == newItem.id
 
-        override fun areItemsTheSame(oldItem: Estate, newItem: Estate): Boolean = oldItem == newItem
+        override fun areItemsTheSame(oldItem: EstateListItemViewState, newItem: EstateListItemViewState): Boolean = oldItem == newItem
 
     }
 
