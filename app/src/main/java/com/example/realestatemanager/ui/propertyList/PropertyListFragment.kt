1e7e33d01732ba23d940c23ee2e2ff6797b35559
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-11-07 17:21:41 +0100

    feat :   get data from viewModel and populate recycler

--- app/src/main/java/com/example/realestatemanager/ui/propertyList/PropertyListFragment.kt
+++ app/src/main/java/com/example/realestatemanager/ui/propertyList/PropertyListFragment.kt
@@ -5,22 +5,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.realestatemanager.databinding.PropertyDetailFragmentBinding
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.example.realestatemanager.databinding.PropertyListFragmentBinding
+import dagger.hilt.android.AndroidEntryPoint
 
 
+@AndroidEntryPoint
 class PropertyListFragment : Fragment() {
     private var _binding: PropertyListFragmentBinding? = null
     private val binding get() = _binding!!
+
+
+    private val viewModel by viewModels<PropertyListViewModel>()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = PropertyListFragmentBinding.inflate(inflater,container,false)
+        _binding = PropertyListFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val recyclerView: RecyclerView = binding.root
+        val adapter = EstateListAdapter {
+            viewModel.onEstateClicked(it)
+        }
 
+        recyclerView.adapter = adapter
+        viewModel.estatesLiveData.observe(viewLifecycleOwner) {
+            adapter.submitList(it)
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
