2b94ad0dc25ca5ee78cbac02b5dc6e4a8050fe25
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-11-07 17:22:25 +0100

    feat : get data from repo and transform it into view list item state

--- app/src/main/java/com/example/realestatemanager/ui/propertyList/PropertyListViewModel.kt
+++ app/src/main/java/com/example/realestatemanager/ui/propertyList/PropertyListViewModel.kt
@@ -1,21 +1,48 @@
 package com.example.realestatemanager.ui.propertyList
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
-import com.example.realestatemanager.data.Repositories.EstateRepository
 import com.example.realestatemanager.data.model.Estate
+import com.example.realestatemanager.data.repositories.CurrentEstateIdRepository
+import com.example.realestatemanager.data.repositories.EstateRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class PropertyListViewModel @Inject constructor(private val estateRepository: EstateRepository) :
+@HiltViewModel
+class PropertyListViewModel @Inject constructor(
+    private val estateRepository: EstateRepository,
+    private val currentEstateIdRepository: CurrentEstateIdRepository
+) :
     ViewModel() {
 
-        val allEstate: LiveData<List<Estate>> = estateRepository.allEstate.asLiveData()
 
-        fun insertEstate(estate: Estate) = viewModelScope.launch {
-            estateRepository.insert(estate)
-        }
+    val estatesLiveData: LiveData<List<EstateListItemViewState>> =
+        estateRepository.getEstates().map { estates ->
+            Log.e("listsize",estates.size.toString())
+
+            estates.map {
+                EstateListItemViewState(
+                    it.id,
+                    it.pictures!![0],
+                    it.price,
+                    it.type,
+                    it.location[2]
+                )
+            }
+        }.asLiveData()
+
+    fun onEstateClicked(id: Int) {
+        currentEstateIdRepository.setCurrentId(id)
+    }
+
+     fun addEstate(estate: Estate) = viewModelScope.launch {
+        estateRepository.insertEstate(estate)
+    }
+
 
 }
