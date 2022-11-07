3a0b3c191a6b162273613181a4cb81ac5cde6e0c
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-11-07 17:17:29 +0100

    ref change nullability to certain property

--- app/src/main/java/com/example/realestatemanager/data/model/Estate.kt
+++ app/src/main/java/com/example/realestatemanager/data/model/Estate.kt
@@ -1,24 +1,27 @@
 package com.example.realestatemanager.data.model
 
+
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.serialization.Serializable
 
+
 @Serializable
 @Entity(tableName = "estate")
 data class Estate(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "name") val type: String,
+    @ColumnInfo(name = "price") val price : String,
     @ColumnInfo(name = "surface") val surface: Int,
+    @ColumnInfo(name = "location") val location : List<String>,
     @ColumnInfo(name = "number_of_rooms") val numberOfRooms: Int,
     @ColumnInfo(name = "number_of_bathrooms")  val numberOfBathrooms: Int,
-    @ColumnInfo(name = "pictures") val pictures: List<String> = listOf(),
-    @ColumnInfo(name = "address") val address: String,
-    @ColumnInfo(name = "poi") val poi: List<String> = listOf(),
+    @ColumnInfo(name = "pictures") val pictures: List<String>?,
+    @ColumnInfo(name = "poi") val poi: List<String>?,
     @ColumnInfo(name = "status") val status: Boolean,
     @ColumnInfo(name ="entry_date") val entryDate: String,
-    @ColumnInfo(name = "saleDate") val saleDate: String,
+    @ColumnInfo(name = "saleDate") val saleDate: String?,
     @ColumnInfo(name = "estateManagerName") val estateManagerName: String
 
 )
