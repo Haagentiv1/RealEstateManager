09a8197eddc615611a30728f23b089e3b7a3b1fb
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-11-07 17:30:45 +0100

    feat : set detail view state

--- app/src/main/java/com/example/realestatemanager/ui/propertyDetail/PropertyDetailViewState.kt
+++ app/src/main/java/com/example/realestatemanager/ui/propertyDetail/PropertyDetailViewState.kt
@@ -1,2 +1,16 @@
 package com.example.realestatemanager.ui.propertyDetail 
 
+data class PropertyDetailViewState(
+    val id : Int,
+    val picturesList: List<String>,
+    val description : String,
+    val surface : Int,
+    val numberOfRooms : Int,
+    val numberOfBedRooms : Int,
+    val numberOfBathRooms : Int,
+    val address : String,
+    val town : String,
+    val addressNumber : String,
+    val zipCode :String,
+    val country : String
+)
