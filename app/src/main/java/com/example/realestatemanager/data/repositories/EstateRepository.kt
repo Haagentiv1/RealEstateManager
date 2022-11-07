43a4dad5d0434ed039fea45e8d312f9444e6ade5
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-11-07 21:06:11 +0100

    ref

--- app/src/main/java/com/example/realestatemanager/data/repositories/EstateRepository.kt
+++ app/src/main/java/com/example/realestatemanager/data/repositories/EstateRepository.kt
@@ -1,19 +1,22 @@
 package com.example.realestatemanager.data.repositories
 
-import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
 import com.example.realestatemanager.data.Dao.EstateDao
 import com.example.realestatemanager.data.model.Estate
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
-import javax.inject.Singleton
 
-interface EstateRepository {
+class EstateRepository @Inject constructor(private val estateDao: EstateDao) {
 
-    fun getEstates(): Flow<List<Estate>>
+    val estates: Flow<List<Estate>> = estateDao.getEstates()
 
-    suspend fun getEstateById(id : Int) : Estate?
+    suspend fun getEstateById(id: Int) : LiveData<Estate?>{
+        return estateDao.getEstateById(id)
+    }
 
-    suspend fun insertEstate(estate: Estate)
+    suspend fun insertEstate(estate: Estate) {
+        estateDao.insertEstate(estate)
+    }
 
 
 }
