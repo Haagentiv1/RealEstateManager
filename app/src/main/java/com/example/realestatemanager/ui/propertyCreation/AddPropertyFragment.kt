756f2afa2784bdb08ff23841cfcad3bc9bca80d0
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-11-15 12:16:47 +0100

    Feat  : Manage picture captured in imageView

--- app/src/main/java/com/example/realestatemanager/ui/propertyCreation/AddPropertyFragment.kt
+++ app/src/main/java/com/example/realestatemanager/ui/propertyCreation/AddPropertyFragment.kt
@@ -1,14 +1,21 @@
 package com.example.realestatemanager.ui.propertyCreation
 
+import android.content.Context.MODE_PRIVATE
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.realestatemanager.databinding.AddPropertyFragmentBinding
+import java.io.IOException
 
 
 class AddPropertyFragment : Fragment() {
+
 
     private var _binding: AddPropertyFragmentBinding? = null
     private val binding get() = _binding!!
@@ -26,9 +33,34 @@ class AddPropertyFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        val takePicture =
+            registerForActivityResult(ActivityResultContracts.TakePicturePreview()) {
+                Glide.with(this).load(it).into(binding.addPropertyIvPropertyPicture)
+            }
+
+        binding.addPropertyBtnAddImage.setOnClickListener {
+            takePicture.launch()
+        }
     }
 
-    fun tkePicture(){
 
+    private fun savePhotoToInternalStorage(filename: String, bmp: Bitmap): Boolean {
+        return try {
+            context?.openFileOutput("$filename.jpg", MODE_PRIVATE).use { stream ->
+                if (!bmp.compress(Bitmap.CompressFormat.JPEG, 95, stream)) {
+                    throw IOException("Couldn't save bitmap")
+                }
+            }
+            true
+        } catch (e: IOException) {
+            e.printStackTrace()
+            false
+        }
     }
+
+
 }
+
+
