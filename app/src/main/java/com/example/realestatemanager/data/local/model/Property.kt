014e4ec958a88ed38e10263ae5dff025a086949b
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-11-19 03:30:26 +0100

    ref

--- app/src/main/java/com/example/realestatemanager/data/local/model/Property.kt
+++ app/src/main/java/com/example/realestatemanager/data/local/model/Property.kt
@@ -19,11 +19,10 @@ data class Property(
     @ColumnInfo(name = "number_of_rooms") val numberOfRooms: Int,
     @ColumnInfo(name = "number_of_bedrooms") val numberOfBedRooms: Int,
     @ColumnInfo(name = "number_of_bathrooms") val numberOfBathRooms: Int,
-    @ColumnInfo(name = "pictures") val pictures: List<Pair<String,String>>,
+    @ColumnInfo(name = "pictures") val pictures: List<Pair<String, String>>,
     @ColumnInfo(name = "poi") val poi: List<String>?,
     @ColumnInfo(name = "status") val status: Boolean,
-    @ColumnInfo(name ="entry_date") val entryDate: String,
+    @ColumnInfo(name = "entry_date") val entryDate: String,
     @ColumnInfo(name = "saleDate") var saleDate: String?,
     @ColumnInfo(name = "estateManagerName") val estateManagerName: String
-
 )
