0c25dec19d09bd854524a37c66b4f592000c8328
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-11-21 18:12:21 +0100

    Feat : loan business logic test

--- app/src/main/java/com/example/realestatemanager/ui/realEstateLoan/LoanFragment.kt
+++ app/src/main/java/com/example/realestatemanager/ui/realEstateLoan/LoanFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.realestatemanager.databinding.FragmentLoanBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.roundToInt
 
 @AndroidEntryPoint
 class LoanFragment : Fragment() {
@@ -25,4 +27,35 @@ class LoanFragment : Fragment() {
         _binding = FragmentLoanBinding.inflate(inflater)
         return  binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.loanBtnSum.setOnClickListener {
+            if (checkEtState()){
+                binding.loanTvLoanResult.text = sumLoan()
+            }
+            else {
+                Toast.makeText(context,"Some fields are empty",Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun sumLoan(): String = (((binding.loanEtPrice.text.toString().toInt() *
+            binding.loanEtTaxe.text.toString().toFloat()) -
+            binding.loanEtApport.text.toString().toInt())
+            / (binding.loanEtTime.text.toString().toInt() * 12)).roundToInt().toString()
+
+    private fun checkEtState(): Boolean {
+        val listOfEt = listOf(binding.loanEtPrice.text,binding.loanEtTaxe.text,binding.loanEtApport.text,binding.loanEtTime.text)
+        var result = true
+        listOfEt.forEach {
+            if (it.isNullOrBlank()){
+                result = false
+            }
+        }
+        return result
+    }
+
+
 }
